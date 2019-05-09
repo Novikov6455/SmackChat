f672bc10db1730f3f9802259c021827a5b330d5e
vadimnovikov <vadim.novikov68@icloud.com>
2019-05-08 23:18:18 -0700

    cleaned the cod

--- app/src/main/java/com/example/smack/MainActivity.kt
+++ app/src/main/java/com/example/smack/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.example.smack
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
-
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
