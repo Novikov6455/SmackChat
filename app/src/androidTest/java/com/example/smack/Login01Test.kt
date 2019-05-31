264b956acbcd69e650b804a5309c29ab20a91585
vadimnovikov <vadim.novikov68@icloud.com>
2019-05-30 18:10:27 -0700

    made readable login logout test logic

--- app/src/androidTest/java/com/example/smack/Login01Test.kt
+++ app/src/androidTest/java/com/example/smack/Login01Test.kt
@@ -2,22 +2,14 @@ package com.example.smack
 
 
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.Espresso.pressBack
 import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import android.view.View
-import android.view.ViewGroup
 import com.example.smack.Controller.MainActivity
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
-import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -30,183 +22,78 @@ class Login01Test {
     @Rule
     @JvmField
     var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
-
     @Test
-    fun login01Test() {
-        val appCompatImageButton = onView(
+    fun loginLogoutBaseTest() {
+        // tap toolbar button
+        val toolbarBtn = onView(
             allOf(
                 withContentDescription("Open navigation drawer"),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.toolbar),
-                        childAtPosition(
-                            withClassName(`is`("android.support.design.widget.AppBarLayout")),
-                            0
-                        )
-                    ),
-                    1
-                ),
+//                withId(R.id.toolbar),
                 isDisplayed()
             )
         )
-        appCompatImageButton.perform(click())
-        sleep(1000)
+        toolbarBtn.perform(click())
+        sleep(3000)
 
-        val appCompatButton = onView(
+        // tap login button
+        val navHeaderLoginBtn = onView(
             allOf(
                 withId(R.id.loginBtnNavHeader), withText("Login"),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.nav_drawer_header_include),
-                        childAtPosition(
-                            withClassName(`is`("android.widget.RelativeLayout")),
-                            0
-                        )
-                    ),
-                    3
-                ),
                 isDisplayed()
             )
         )
-        appCompatButton.perform(click())
+        navHeaderLoginBtn.perform(click())
+        sleep(1000)
 
-        val appCompatEditText = onView(
+        // type email address
+        val loginEmailTxt = onView(
             allOf(
                 withId(R.id.loginEmailTxt),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    0
-                ),
                 isDisplayed()
             )
         )
-        appCompatEditText.perform(click())
+        loginEmailTxt.perform(replaceText("o@o.com"), closeSoftKeyboard())
 
-        val appCompatEditText2 = onView(
-            allOf(
-                withId(R.id.loginEmailTxt),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    0
-                ),
-                isDisplayed()
-            )
-        )
-        appCompatEditText2.perform(replaceText("o@o.com"), closeSoftKeyboard())
-
-        val appCompatEditText3 = onView(
+        // type password
+        val loginPasswordTxt = onView(
             allOf(
                 withId(R.id.loginPasswordTxt),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    1
-                ),
                 isDisplayed()
             )
         )
-        appCompatEditText3.perform(replaceText("123456"), closeSoftKeyboard())
+        loginPasswordTxt.perform(replaceText("123456"), closeSoftKeyboard())
 
-//        pressBack()
-        sleep(1000)
-
-        val appCompatButton2 = onView(
+        // tap login button at Login Screen/layout
+        val loginScreenLoginBtn = onView(
             allOf(
                 withId(R.id.loginLoginBtn), withText("login"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    2
-                ),
                 isDisplayed()
             )
         )
+        loginScreenLoginBtn.perform(click())
         sleep(1000)
-        appCompatButton2.perform(click())
 
+        // check what user name is displayed
         val textView = onView(
             allOf(
                 withId(R.id.userNameNavHeader), withText("Olga"),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.nav_drawer_header_include),
-                        childAtPosition(
-                            IsInstanceOf.instanceOf(android.widget.RelativeLayout::class.java),
-                            0
-                        )
-                    ),
-                    1
-                ),
                 isDisplayed()
             )
         )
         sleep(3000)
         textView.check(matches(withText("Olga")))
-        val appCompatButton3 = onView(
+
+        // teardown part of test: user logout
+        val navHeaderLogoutBtn = onView(
             allOf(
                 withId(R.id.loginBtnNavHeader), withText("Logout"),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.nav_drawer_header_include),
-                        childAtPosition(
-                            withClassName(`is`("android.widget.RelativeLayout")),
-                            0
-                        )
-                    ),
-                    3
-                ),
                 isDisplayed()
             )
         )
-        appCompatButton3.perform(click())
+        navHeaderLogoutBtn.perform(click())
         Thread.sleep(1000)
 
-
-        val button = onView(
-            allOf(
-                withId(R.id.loginBtnNavHeader),
-                childAtPosition(
-                    allOf(
-                        withId(R.id.nav_drawer_header_include),
-                        childAtPosition(
-                            IsInstanceOf.instanceOf(android.widget.RelativeLayout::class.java),
-                            0
-                        )
-                    ),
-                    3
-                ),
-                isDisplayed()
-            )
-        )
-        button.check(matches(isDisplayed()))
-    }
-
-
-    private fun childAtPosition(
-        parentMatcher: Matcher<View>, position: Int
-    ): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
+        // check what Navigator Header Screen login button is displayed
+        navHeaderLoginBtn.check(matches(isDisplayed()))
     }
 }
